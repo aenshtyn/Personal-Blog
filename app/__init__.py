b81c3e8dd268bb05392aac0ec1503467ec0027e9
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-05-07 16:45:52 +0300

    added bootstrat to page

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
 def create_app(config_name):
 
-    app = Flask(__name__)
+# Initializing application
+app = Flask(__name__,instance_relative_config = True)
 
-    # Creating the app configurations
-    app.config.from_object(config_options[config_name])
+# Setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile("config.py")
 
-    # Initializing flask extensions
-    db.init_app(app)
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 
 from app import views
