4f4e706ffa9dca6822d6b7904e7e3b17f04e6bb4
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-05-09 17:59:08 +0300

    created functions that wuill get the quotes from the api

--- app/requests.py
+++ app/requests.py
@@ -1,1 +1,34 @@
+from app import app
+import urllib.request,json
+from .models import quote
 
+Quote = quote.Quote
+
+base_url = app.config["QUOTES_URL"]
+
+def get_quotes():
+
+    get_quotes_url = base_url.format()
+
+    with urllib.request.urlopen(get_quotes_url) as url:
+        get_quotes.data = url.read()
+        get_quotes_response = json.loads(get_quotes data)
+
+        quotes_results = None
+
+        if get_quotes_response['results']:
+            quotes_results_list = get_quotes_response['results']
+            quotes_results = process_results(quote_results_list)
+
+    return quote_results
+
+def process_results(quote_list):
+
+    quote_results = []
+    for quote_item in quote_list:
+        quote = quote_item.get('quote')
+        author = quote_item.get('author')
+
+        quote_results.append(quote_object)
+
+    return quote_results
