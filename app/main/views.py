d594f0188c189486199b3f9c4a154d7a662f4660
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-05-09 20:59:20 +0300

    created a model class for the roles

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..models import User, Post
+from ..models import User, Post, Quote
 from .. import db
 from ..requests import get_quotes
 
@@ -20,11 +20,13 @@ def index():
     posts = Post.query.filter_by(category = 'World News').all()
 
 
-    random_quotes = get_quotes()
-    print(random_quotes)
+    # random_quotes = get_quotes()
+    # print(random_quotes)
 
     '''
     View root page function that returns the index page and its data
     '''
     title = 'Home - Welcome to Aenshtyns Blog'
-    return render_template('index.html', title = title, random_quotes = random_quotes, users = users, posts = posts)
+    return render_template('index.html', title = title, users = users, posts = posts)
+
+    # return render_template('index.html', title = title, random_quotes = random_quotes, users = users, posts = posts)
