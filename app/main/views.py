b67428556d256b8eb278247df5d76775f43940ff
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-05-10 11:53:05 +0300

    created a user profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for, abort
 from . import main
 from ..models import User, Post, Quote
 from .. import db
 from ..requests import get_quotes
 from flask_login import login_required
+from .forms import ReviewForm,UpdateProfile
 
 # @main.route('/')
 # def index():
@@ -38,3 +39,32 @@ def index():
 def new_post(id):
 
        return render_template('new_blog.html',title = title)
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
